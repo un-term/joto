8aaff5364378411c55fe512e9bd2d36cfb5c7afc
Euan Muir <unterm@mailterm.co.uk>
2023-10-07 16:50:32 +0100

    Add html tests which check html content

--- test_joto.py
+++ test_joto.py
@@ -15,20 +15,20 @@ parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir) 
 import joto
 
+def check_string(file_path, stringFind):
+    with open(file_path, 'r') as file:
+        content = file.read()
+        if stringFind in content:
+            return True
+        else:
+            return False
+
 class TestSQLiteDB(unittest.TestCase):
 
-    def test_all(self):
+    def test_add_new_entry1(self):
         print("------------------------------------------")
-        print("Add new entry")
+        print("Add new entry:date, image, text")
         print("------------------------------------------")
-
-        image1 = "2021-04-21_parrots.jpg"
-        image2 = "2021-01-02_fox.jpg"
-        image3 = "2021-03-04_cow.jpg"
-        image4 = "2020-11-10_giraffe.jpg"
-        image5 = "2021-01-02_bird.jpg"
-        html_template = "template.html"
-        size = "1000x1000"
 
         if not os.path.exists("test_workspace"):
             os.makedirs("test_workspace")        
@@ -36,7 +36,7 @@ class TestSQLiteDB(unittest.TestCase):
         if os.path.isfile("test_config.json"):
             json_config = joto.JsonConfig("test_config.json")
         else:
-            raise Exception("config file required") 
+            raise Exception("config file equired") 
 
         sqlite_db = joto.JotoSQLiteDB(json_config.sqlite_db_path)
         images_manage = joto.ImagesManage(json_config.image_size, json_config.original_image_dirpath, json_config.compressed_image_dirpath)
@@ -47,46 +47,58 @@ class TestSQLiteDB(unittest.TestCase):
         joto_obj.create_req()
         joto_obj.check_req()
 
-        joto_obj.add_new_entry("2023-03-04", "This is a cow", "./test_data/2021-03-04_cow.jpg")
+        joto_obj.add_new_entry("2021-03-04", "This is a cow", "./test_data/2021-03-04_cow.jpg")
         joto_obj.create_content()
         joto_obj.write_content()
 
-        joto_obj.add_new_entry("2023-05-26", "Giraffing around", "./test_data/2020-11-10_giraffe.jpg")
+        count = 0
+        if check_string("test_workspace/joto.html",'<h1>2021-03-04</h1>'):
+            count += 1
+        if check_string("test_workspace/joto.html",'<img src="test_workspace/images/compressed/2021-03-04_cow.jpg">'):
+            count += 1
+        if check_string("test_workspace/joto.html",'<p>This is a cow</p>'):
+            count += 1
+            
+        self.assertEqual(3, count)
+
+
+    def test_add_new_entry2(self):
+        print("")
+        print("------------------------------------------")
+        print("Add new entry:date, text, no image")
+        print("------------------------------------------")
+
+        if not os.path.exists("test_workspace"):
+            os.makedirs("test_workspace")        
+        # load config and setup objects
+        if os.path.isfile("test_config.json"):
+            json_config = joto.JsonConfig("test_config.json")
+        else:
+            raise Exception("config file equired") 
+
+        sqlite_db = joto.JotoSQLiteDB(json_config.sqlite_db_path)
+        images_manage = joto.ImagesManage(json_config.image_size, json_config.original_image_dirpath, json_config.compressed_image_dirpath)
+        html = joto.HTML("./templates/output.html", json_config.html_output_path, json_config.compressed_image_dirpath)
+        joto_obj = joto.Joto(sqlite_db, images_manage, html)
+
+        joto_obj.delete_req()
+        joto_obj.create_req()
+        joto_obj.check_req()
+
+        joto_obj.add_new_entry("2021-01-02", "", "./test_data/2021-01-02_fox.jpg")
         joto_obj.create_content()
         joto_obj.write_content()
 
-        joto_obj.add_new_entry("2023-07-26", "no image", "")
-        joto_obj.create_content()
-        joto_obj.write_content()
+        count = 0
+        if check_string("test_workspace/joto.html",'check_string("test_workspace/joto.html","<h1>2021-01-02</h1>")'):
+            count += 1
+        if check_string("test_workspace/joto.html",'<img src="test_workspace/images/compressed/2021-01-02_fox.jpg">'):
+            count += 1
+        if check_string("test_workspace/joto.html",'<p></p>'):
+            count += 1
+            
+        self.assertEqual(2, count)
 
-        # Scanning directory
-        # joto_obj.scan_for_and_add_images_with_text(src_dir)
-        # Test deleting - scan order dependent on filename - last fat_Sox
-        # joto_obj.delete_entry(1)
-
-        # text_input.get_input.return_value = "2020-02-12"
-        # joto_obj.add_text_only()
-        # joto_obj.delete_last_entry()
-
-        # Test text for same date
-        # text_input.get_input.return_value = "2020-02-12"
-        # joto_obj.add_text_only()
-        # text_input.get_input.return_value = "2020-02-12"
-        # joto_obj.add_text_only()
-
-        # Add image using path
-        # joto_obj.add_image_from_path(img_path)
-
-        # joto_obj.create_content()
-        # joto_obj.write_content()
-
-        # all_data = sqlite_db.retrieve_all_data_ordered_by_date()
-        # result = False
-        # if all_data[0][3] == image1: # first dated image
-        #     result = True
-        # joto_obj.scan_for_and_add_images_with_text()
-
-        self.assertTrue(True)
 
 if __name__ == '__main__':
     unittest.main()
